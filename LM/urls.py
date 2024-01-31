a2fc94df7cfad294e012aeb16a146c33f90c3a4a
Matthan Cantrell <cantrellmatthan247@gmail.com>
2024-01-30 20:35:58 -0700

    Pushing Image Fix. Will further comment

--- LM/urls.py
+++ LM/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.contrib.staticfiles import views
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
@@ -24,7 +24,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        path(r'^static/(?P<path>.*)$', views.static.serve, {'document_root': settings.STATIC_ROOT}, name="static"),
-        path(r'^media/(?P<path>.*)$', views.static.serve, {'document_root': settings.MEDIA_ROOT}, name="media")
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
